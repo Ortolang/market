1f3ee7602fd878a65cf5e9cb43c4931679fbbfcc
Céline Moro <cmoro@atilf.fr>
2014-09-30 12:04:55 +0000

    redirect url after login

---
+++ app/scripts/services/param.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ortolangMarketApp.param
+ * @description
+ * # param
+ * Value in the ortolangMarketApp.
+ */
+angular.module('ortolangMarketApp')
+    .value('param', {
+        redirectToUrlAfterLogin: '/'
+    });
