1c5b0dcf04ae438d5d2947825e9b000a8e3ff019
Céline Moro <cmoro@atilf.fr>
2014-10-02 12:25:20 +0000

    cookiefactry.j did not commit

---
+++ app/scripts/services/cookiefactory.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ortolangMarketApp.CookieFactory
+ * @description
+ * # CookieFactory
+ * Factory in the ortolangMarketApp.
+ */
+angular.module('ortolangMarketApp')
+    .factory('CookieFactory', function ($q, $timeout) {
+        return {
+            /**
+             * Get a cookie by its name
+             * @param name
+             * @returns {*}
+             */
+            getCookie: function (name) {
+                var deferred = $q.defer();
+                // Used to un-stringify value of cookie
+                $.cookie.json = true;
+
+                $timeout(function () {
+                    deferred.resolve($.cookie(name));
+                }, 0);
+
+                return deferred.promise;
+            },
+
+            /**
+             * Get all cookies
+             * @returns {undefined}
+             */
+            getAllCookies: function () {
+                // Used to un-stringify value of cookie
+                $.cookie.json = true;
+                return $.cookie();
+            },
+
+            /**
+             * Create a cookie
+             * @param name
+             * @param value
+             * @param expiration
+             * @returns {undefined}
+             */
+            setCookie: function (name, value, expiration) {
+                expiration = angular.isDefined(expiration) ? expiration : 7;
+                // Used to stringify value of cookie
+                $.cookie.json = true;
+                return $.cookie(name, value, { expires: expiration });
+            },
+
+            /**
+             * Delete a cookie by its name
+             * @param name
+             * @returns {*}
+             */
+            deleteCookie: function (name) {
+                return $.removeCookie(name);
+            }
+        };
+    });
