e206c1d4fb6d4d60a614422f245d429f155e0cae
Cyril Pestel <cpestel@atilf.fr>
2014-10-09 14:51:36 +0000

    Removes name field Made some form validation stuff

--- app/scripts/controllers/metadata-creator-market-ortolang.js
+++ app/scripts/controllers/metadata-creator-market-ortolang.js
@@ -12,6 +12,15 @@ angular.module('ortolangMarketApp')
     console.debug('MetadataCreatorMarketOrtolangCtrl')
     
 	$scope.categories = [{id:'corpora', label:'Corpus'}, {id:'lexicon', label:'Lexique'}];
-	$scope.use_conditions = [{id:'free', label:'Libre'}, {id:'free-nc', label:'Libre sans usage commercial'}, {id:'restricted', label:'Privé'}];
+	$scope.use_conditions = [{id:'free', label:'Libre'}, {id:'free-nc', label:'Libre sans usage commercial'}, {id:'restricted', label:'Restreint'}];
 
+	$scope.submit = function(form) {
+		console.debug("submit form : ");
+		console.debug(form);
+
+		if (form.$invalid) {
+			console.debug('not ready');
+		    return;
+		  }
+	}
   });
