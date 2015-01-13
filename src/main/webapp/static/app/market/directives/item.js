b218b8b868f073faf7e9d85cb322aded38489681
cyrilpestel <cyril.pestel@gmail.com>
2015-01-13 09:21:03 +0100

    Solves bug on the sort of items by creating a directive ‘item’

---
+++ src/main/webapp/static/app/market/directives/item.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ortolangMarketApp.directive:item
+ * @description
+ * # item
+ * Directive of the ortolangMarketApp
+ */
+angular.module('ortolangMarketApp')
+    .directive('item', [ '$location', 'ObjectResource', 'DownloadResource', 'N3Serializer',  function ($location, ObjectResource, DownloadResource, N3Serializer) {
+        return {
+            restrict: 'EA',
+            scope: {
+                key: '='
+            },
+            templateUrl: 'market/directives/item.html',
+            link: {
+                post : function (scope) {
+
+                    scope.clickItem = function (key) {
+                        $location.path('/market/' + key);
+                    };
+
+                    // Loads properties of each object
+                    ObjectResource.get({oKey: scope.key}).$promise
+                        .then(function (oobject) {
+
+                            if (oobject.object.root === true) {
+                                if (oobject.object.metadatas.length > 0) {
+                                    
+                                    var metaKey = oobject.object.metadatas[0].key;
+
+                                    DownloadResource.download({oKey: metaKey}).success(function (metaContent) {
+                                        N3Serializer.fromN3(metaContent).then(function (data) {
+                                            scope.oobject = oobject;
+                                            scope.meta = data;
+                                            if(data['http://www.ortolang.fr/ontology/image']) {
+                                                
+                                                ObjectResource.element({oKey: scope.key, path: data['http://www.ortolang.fr/ontology/image']}).$promise.then(function(oobject) {
+                                                    scope.image = DownloadResource.getDownloadUrl({oKey: oobject.key});
+                                                }, function (reason) {
+                                                    console.error(reason);
+                                                });
+                                            }
+
+                                        });
+                                    }).error(function (error) {
+                                        console.error('error during process : ' + error);
+                                    });
+                                }
+                            }
+                        }, function (reason) {
+                            console.error(reason);
+                    });
+                }
+            }
+        };
+    }]);
--- src/main/webapp/static/app/market/market-home.js
+++ src/main/webapp/static/app/market/market-home.js
@@ -8,7 +8,7 @@
  * Controller of the ortolangMarketApp
  */
 angular.module('ortolangMarketApp')
-    .controller('MarketHomeCtrl', ['$scope', '$location', 'ObjectResource', 'DownloadResource', 'N3Serializer', function ($scope, $location, ObjectResource, DownloadResource, N3Serializer) {
+    .controller('MarketHomeCtrl', ['$scope', '$location', 'ObjectResource', function ($scope, $location, ObjectResource) {
 
         $scope.search = function () {
             if ($scope.content !== '') {
@@ -16,49 +16,12 @@ angular.module('ortolangMarketApp')
             }
         };
 
-        $scope.clickItem = function (item) {
-            $location.path('/market/' + item.oobject.key);
-        };
-
         function loadObjects() {
             // Loads all objects
             ObjectResource.get({items: 'true', status: 'PUBLISHED'}).$promise.then(function (oobjects) {
 
                 angular.forEach(oobjects.entries, function (entry) {
-
-                    // Loads properties of each object
-                    ObjectResource.get({oKey: entry}).$promise
-                        .then(function (oobject) {
-
-                            if (oobject.object.root === true) {
-                                if (oobject.object.metadatas.length > 0) {
-                                    
-                                    var metaKey = oobject.object.metadatas[0].key;
-
-                                    DownloadResource.download({oKey: metaKey}).success(function (metaContent) {
-                                        N3Serializer.fromN3(metaContent).then(function (data) {
-                                            var item = {};
-                                            item.oobject = oobject;
-                                            item.meta = data;
-                                            if(data['http://www.ortolang.fr/ontology/image']) {
-                                                
-                                                ObjectResource.element({oKey: entry, path: data['http://www.ortolang.fr/ontology/image']}).$promise.then(function(oobject) {
-                                                    item.image = DownloadResource.getDownloadUrl({oKey: oobject.key});
-                                                }, function (reason) {
-                                                    console.error(reason);
-                                                });
-                                            }
-                                            $scope.items.push(item);
-
-                                        });
-                                    }).error(function (error) {
-                                        console.error('error during process : ' + error);
-                                    });
-                                }
-                            }
-                        }, function (reason) {
-                            console.error(reason);
-                        });
+                    $scope.items.push(entry);
                 });
             });
         }
