3385594ab033790abf0e9e87cb9fafd444d34c06
cyril pestel <cyril@ysatis.atilf.fr>
2015-01-21 12:41:43 +0100

    Adds metadata for online tools Loads tools list from objects REST API

--- src/main/webapp/static/app/workspace/tools-list.js
+++ src/main/webapp/static/app/workspace/tools-list.js
@@ -8,7 +8,7 @@
  * Controller of the ortolangMarketApp
  */
 angular.module('ortolangMarketApp')
-    .controller('ToolsListCtrl', ['$scope', function ($scope) {
+    .controller('ToolsListCtrl', ['$scope', 'ObjectResource', 'DownloadResource', 'N3Serializer', function ($scope, ObjectResource, DownloadResource, N3Serializer) {
 
         // ***************** //
         // Editor visibility //
@@ -52,28 +52,72 @@ angular.module('ortolangMarketApp')
             return $scope.selectedTool !== undefined;
         };
 
+        $scope.loadToolsList = function() {
+            // $scope.tools = [
+            //     {id: 'tika', name: 'Tika', description: 'Detects and extracts metadata and text content.'},
+            //     {id: 'sample-tool', name: 'Sample tool', description: 'Dumb tool for demonstration purpose.'}
+            // ];
+            // Loads all objects
+            ObjectResource.get({items: 'true', status: 'PUBLISHED'}).$promise.then(function (oobjects) {
+                var index = 0;
+                var items = [];
+                angular.forEach(oobjects.entries, function (entry) {
+                    items.push({key: entry, rang: index});
+                    index++;
+                });
+
+                loadMetadata(items);
+            });
+        };
+        
+        function loadMetadata(items) {
+
+            angular.forEach(items, function (item) {
+                // Loads properties of each object
+                ObjectResource.get({oKey: item.key}).$promise
+                    .then(function (oobject) {
+
+                        if (oobject.object.root === true) {
+                            if (oobject.object.metadatas.length > 0) {
+                                
+                                var metaKey = oobject.object.metadatas[0].key;
+
+                                DownloadResource.download({oKey: metaKey}).success(function (metaContent) {
+                                    N3Serializer.fromN3(metaContent).then(function (data) {
+                                       
+                                        if ( data['http://www.ortolang.fr/ontology/type'] && data['http://www.ortolang.fr/ontology/type']==='Outil') {
+                                            item.id = data['http://www.ortolang.fr/ontology/toolId'];
+                                            item.name = data['http://purl.org/dc/elements/1.1/title'];
+                                            item.description = data['http://purl.org/dc/elements/1.1/description'];
+                                            item.meta = data;
+
+                                            $scope.tools.push(item);
+                                        }
+                                    });
+                                }).error(function (error) {
+                                    console.error('error during process : ' + error);
+                                });
+                            }
+                        }
+                    }, function (reason) {
+                        console.error(reason);
+                });
+            });
+        }
 
         // ********* //
         // Listeners //
         // ********* //
 
         $scope.$on('tools-list-show', function () {
-            // resetMetadata();
-            // $scope.userMetadataFormat = metadataFormat;
-            // $scope.metadataForm = metadataFormat.view;
+            $scope.resetSelectedTool();
             $scope.show();
-        });
-
-        $scope.$on('tools-list-create', function () {
-            // sendForm(content, contentType);
         });
 
 
         function init() {
-            $scope.tools = [
-                {id: 'tika', name: 'Tika', description: 'Detects and extracts metadata and text content.'},
-                {id: 'sample-tool', name: 'Sample tool', description: 'Dumb tool for demonstration purpose.'}
-            ];
+            $scope.tools = [];
+            $scope.loadToolsList();
             $scope.selectedTool = undefined;
 
         }
