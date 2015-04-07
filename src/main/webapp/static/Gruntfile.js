ccb0f1152cbc81ae6ca1a4876c44c0fdff9b11a2
Frédéric Pierre <fpierre@atilf.fr>
2015-04-07 16:19:22 +0200

    - Configure jshint to not fail on warnings

--- src/main/webapp/static/Gruntfile.js
+++ src/main/webapp/static/Gruntfile.js
@@ -124,7 +124,8 @@ module.exports = function (grunt) {
         jshint: {
             options: {
                 jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
+                reporter: require('jshint-stylish'),
+                force: true
             },
             all: {
                 src: [
