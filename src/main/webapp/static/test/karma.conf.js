7be77d725f58587d49c8507c155976c01ef573ed
cyrilpestel <cyril.pestel@gmail.com>
2015-04-16 10:27:31 +0200

    Adds tag input to Karma conf Removes browser sidebar metadata test

--- src/main/webapp/static/test/karma.conf.js
+++ src/main/webapp/static/test/karma.conf.js
@@ -56,6 +56,7 @@ module.exports = function (config) {
             'bower_components/objectpath/lib/ObjectPath.js',
             'bower_components/angular-schema-form/dist/schema-form.js',
             'bower_components/angular-schema-form/dist/bootstrap-decorator.js',
+            'bower_components/ng-tags-input/ng-tags-input.min.js',
             'app/**/*.js',
             'test/mock/**/*.js',
             'test/spec/**/*.js',
