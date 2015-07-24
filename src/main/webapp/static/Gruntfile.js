ac20bd0a2c50c40492791e1b12ecd2f01b7c1b9e
Frédéric Pierre <fpierre@atilf.fr>
2015-07-24 15:43:00 +0200

    - Fix replacement regex

--- src/main/webapp/static/Gruntfile.js
+++ src/main/webapp/static/Gruntfile.js
@@ -507,7 +507,7 @@ module.exports = function (grunt) {
                             replacement: '<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" JSP_ENDTAG'
                         },
                         {
-                            match: /JSP_ENDTAG/,
+                            match: /JSP_ENDTAG/g,
                             replacement: '%>'
                         },
                         {
