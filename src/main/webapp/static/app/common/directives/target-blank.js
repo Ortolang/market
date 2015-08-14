6edcdcb4f451a7d099c01c18d5997fe7a9ed51fc
cyril pestel <cyril@ysatis.atilf.fr>
2015-08-14 12:05:51 +0200

    Adds directive for adding target blank

---
+++ src/main/webapp/static/app/common/directives/target-blank.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ortolangMarketApp.directive:targetBlank
+ * @description
+ * # targetBlank
+ * Directive of the ortolangMarketApp
+ */
+angular.module('ortolangMarketApp')
+    .directive('targetBlank', function () {
+        return {
+        	scope: true,
+        	compile: function(element, attr) {
+        		return function(scope, element, attrs) {
+        			scope.myContent = attrs.content;
+        			scope.$watch(scope.myContent, function (newValue, oldValue){
+				    	if (newValue) {
+							var elems = (element.prop("tagName") === 'A') ? element : element.find('a');
+							elems.attr("target", "_blank");
+				    	}
+        			});
+        		}
+		    }
+        };
+    });
