53a930d3256d980c44cbbf9797baf4c01e5eaaaf
Frédéric Pierre <fpierre@atilf.fr>
2015-11-02 16:04:31 +0100

    - Remove unused property from ortolang-config - Remove node log

--- src/main/webapp/static/app/ortolang-config.js
+++ src/main/webapp/static/app/ortolang-config.js
@@ -1,5 +1,4 @@
 var OrtolangConfig = {};
-OrtolangConfig.logoutRedirectUrl = 'http://localhost:9000/';
 OrtolangConfig.apiServerUrlDefault = 'https://localhost:8443/api';
 OrtolangConfig.apiServerUrlNoSSL = 'http://localhost:8080/api';
 OrtolangConfig.apiSubPath = '/sub';
