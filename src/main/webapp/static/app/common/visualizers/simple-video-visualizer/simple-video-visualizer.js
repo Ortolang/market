c4718567dfa21a7f6497a2e5249cf935f8933249
Frédéric Pierre <fpierre@atilf.fr>
2016-04-19 16:44:53 +0200

    - Improve video visualizer for FLV videos

--- src/main/webapp/static/app/common/visualizers/simple-video-visualizer/simple-video-visualizer.js
+++ src/main/webapp/static/app/common/visualizers/simple-video-visualizer/simple-video-visualizer.js
@@ -61,13 +61,33 @@ angular.module('ortolangVisualizers')
                     }
                     scope.visualizer.content.classes = 'center middle';
                     scope.$on('modal.hide.before', function () {
-                        var videoElement = angular.element('#simple-html5-video');
-                        if (videoElement.length > 0) {
-                            videoElement[0].pause();
-                            videoElement.prop('src', '');
-                            videoElement.remove();
+                        var videoElement;
+                        if (scope.type === 'video/x-flv') {
+                            videoElement = angular.element('.flv-video-player');
+                            if (videoElement.length > 0) {
+                                videoElement[0].SetVariable('player:jsStop', '');
+                                videoElement[0].SetVariable('player:jsUrl', '');
+                                videoElement.remove();
+                            }
+                        } else {
+                            videoElement = angular.element('#simple-html5-video');
+                            if (videoElement.length > 0) {
+                                videoElement[0].pause();
+                                videoElement.prop('src', '');
+                                videoElement.remove();
+                            }
                         }
                     });
+                    if (scope.type === 'video/x-flv') {
+                        var flvPlayer = '<object type="application/x-shockwave-flash" data="vendor/player_flv_maxi.swf" width="640" height="480" class="flv-video-player">';
+                        flvPlayer += '<param name="movie" value="player_flv_maxi.swf" />';
+                        flvPlayer += '<param name="FlashVars" value="flv=' + scope.elements[0].downloadUrl + '&amp;margin=0&amp;showstop=1&amp;showvolume=1&amp;showtime=1&amp;showfullscreen=1" />';
+                        flvPlayer += '<param name="allowFullScreen" value="true" />';
+                        flvPlayer += '</object>';
+                        scope.$on('modal.show', function () {
+                            angular.element(flvPlayer).appendTo('.video-visualizer-wrapper');
+                        });
+                    }
                 }
             }
         };
