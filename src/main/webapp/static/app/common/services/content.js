1a13084859c02d90f7baf2850d3487bd64e228e3
Frédéric Pierre <fpierre@atilf.fr>
2016-06-09 13:27:04 +0200

    - Force login on Content Resource when authenticated (Fixes #744) - Autoplay video and audio (visualizers)

--- src/main/webapp/static/app/common/services/content.js
+++ src/main/webapp/static/app/common/services/content.js
@@ -13,6 +13,9 @@ angular.module('ortolangMarketApp')
         var forceDownloadQueryParam = '?fd=true';
 
         this.getContentUrlWithKey = function (key, login, noSSL) {
+            if (login === false && AuthService.isAuthenticated()) {
+                login = true;
+            }
             return (noSSL ? url.contentNoSSL : url.content) + '/key/' + key + (angular.isDefined(login) ? '?l=' + login : '');
         };
 
