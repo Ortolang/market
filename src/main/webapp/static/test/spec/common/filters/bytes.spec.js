499545ef32942f0555be5678f75310cc6f997015
Frédéric Pierre <fpierre@atilf.fr>
2016-10-07 14:26:08 +0200

    - Fix tests for 'bytes' filter

--- src/main/webapp/static/test/spec/common/filters/bytes.spec.js
+++ src/main/webapp/static/test/spec/common/filters/bytes.spec.js
@@ -21,32 +21,32 @@ describe('Filter: bytes', function () {
 
     it('should round the filesize based on the configured precision', function () {
         var size = 1024 + 512;
-        expect(bytes(size)).toBe('1.5 KB');
-        expect(bytes(size, 2)).toBe('1.50 KB');
+        expect(bytes(size)).toBe('1.5 Ko');
+        expect(bytes(size, 2)).toBe('1.50 Ko');
     });
 
     it('should recognize bytes', function () {
-        expect(bytes(1.000, 0)).toBe('1 bytes');
+        expect(bytes(1.000, 0)).toBe('1 octets');
     });
 
     it('should recognize KiloBytes', function () {
-        expect(bytes(Math.pow(1024, 1), 0)).toBe('1 KB');
+        expect(bytes(Math.pow(1024, 1), 0)).toBe('1 Ko');
     });
 
     it('should recognize MegaBytes', function () {
-        expect(bytes(Math.pow(1024, 2), 0)).toBe('1 MB');
+        expect(bytes(Math.pow(1024, 2), 0)).toBe('1 Mo');
     });
 
     it('should recognize GigaBytes', function () {
-        expect(bytes(Math.pow(1024, 3), 0)).toBe('1 GB');
+        expect(bytes(Math.pow(1024, 3), 0)).toBe('1 Go');
     });
 
     it('should recognize TeraBytes', function () {
-        expect(bytes(Math.pow(1024, 4), 0)).toBe('1 TB');
+        expect(bytes(Math.pow(1024, 4), 0)).toBe('1 To');
     });
 
     it('should recognize PetaBytes', function () {
-        expect(bytes(Math.pow(1024, 5), 0)).toBe('1 PB');
+        expect(bytes(Math.pow(1024, 5), 0)).toBe('1 Po');
     });
 
 });
