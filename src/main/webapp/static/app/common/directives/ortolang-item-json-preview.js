5931cca788abe57dbbc6861034320874e38b67f4
Frédéric Pierre <fpierre@atilf.fr>
2016-10-12 11:49:53 +0200

    - Add wskey to ortolang-item-json-preview directive scope

--- src/main/webapp/static/app/common/directives/ortolang-item-json-preview.js
+++ src/main/webapp/static/app/common/directives/ortolang-item-json-preview.js
@@ -16,6 +16,7 @@ angular.module('ortolangMarketApp')
                 alias: '=',
                 ortolangObject: '=',
                 itemKey: '=',
+                wskey: '=',
                 root: '=',
                 tag: '=',
                 tags: '=',
