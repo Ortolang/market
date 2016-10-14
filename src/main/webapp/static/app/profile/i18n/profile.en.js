25819755deefbf54af22fa4f6cf4658469661cb7
Frédéric Pierre <fpierre@atilf.fr>
2016-10-14 17:38:31 +0200

    - Update co-workers help message

--- src/main/webapp/static/app/profile/i18n/profile.en.js
+++ src/main/webapp/static/app/profile/i18n/profile.en.js
@@ -12,7 +12,7 @@ angular.module('ortolangMarketApp')
         PROFILE: {
             EMPTY: 'empty',
             ADD_FRIEND: 'Add a co-worker',
-            ADD_FRIEND_HELP: 'Adding your co-workers here will give you a quick-access to add them as workspaces members or when describing your resource.',
+            ADD_FRIEND_HELP: 'Adding your co-workers here will give you a quick-access to add them as workspaces members or when describing your resource.<br/>You will also be able to share some pieces of personal information with your co-worker only.',
             ADD_TO_FRIENDS: 'Add to my co-workers',
             ADDED_TO_FRIENDS: 'Added to my co-workers',
             REMOVE_FRIEND: 'Remove from co-workers',
