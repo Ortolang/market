b33310e7c6ab14bd4f14dccd179bc9326af197ce
Frédéric Pierre <fpierre@atilf.fr>
2017-01-03 15:12:54 +0100

    - Fix css animation (ortolang/ortolang#358)

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -39,10 +39,7 @@ module.exports = webpackMerge(commonConfig, {
         new webpack.optimize.DedupePlugin(),
 
         new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
-            mangle: {
-                'screw_ie8': true,
-                'keep_fnames': true
-            }
+            mangle: false
         }),
 
         new ExtractTextPlugin('[name].[hash].css'),
